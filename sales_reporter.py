904892aaf24c2e4239768ed93eb84daf4ec61280
Caroline Feeney <carolinefeeney@Carolines-MacBook-Pro-5.local>
2019-02-11 18:40:30 -0500

    Sorting top sellers

--- sales_reporter.py
+++ sales_reporter.py
@@ -1,5 +1,5 @@
 #sales_reporter.py
-# goal output
+# goal output:
     #SALES REPORT (MARCH 2018)
     #TOTAL SALES: $12,000.71
     #TOP SELLING PRODUCTS:
@@ -7,18 +7,50 @@
         #2. Super Soft Hoodie: $1,875.00
         #3. etc.
 
-#print("SALES REPORT (MONTH YEAR)")
+#source adapted from: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/exercises/sales-reporting/pandas_solution_further.py
 
-import os
+import operator
+import os   
 import csv
-import pandas as pd
+import pandas
+import matplotlib.pyplot as plt
 
+#def to_usd(my_price):
+   # return "${0:,.2f}".format(my_price)  # > $12,000.71
 
 #
 # INFO INPUTS
 #
 
-csv_filename = "sales-201710.csv"
-data = pd.read_csv(csv_filename)
+csv_filename = "sales-201710.csv" #TODO allow user to specify (WITH FILEPATH!)
 
-print(data)
+#csv_filepath = os.path.join(os.path.dirname(__file__), "data", csv_filename) #use this to adapt a file in the data directory
+    # ^ use for project
+csv_data = pandas.read_csv(csv_filename) #read csv into panda dataframe
+
+#
+# CALCULATIONS
+#
+
+monthly_total = csv_data["sales price"].sum()
+
+#get unique products
+product_names = csv_data["product"]
+#print(product_names)
+#print(type(product_names)) > this is a series
+unique_product_names = product_names.unique()
+#print(type(unique_product_names))
+unique_product_names = unique_product_names.tolist() #convert to list because NOT a list
+
+#accumulate total sales per product
+
+top_sellers = []
+
+for product_name in unique_product_names:
+    matching_rows = csv_data[csv_data["product"] == product_name] #define matching_rows as any duplicate
+    product_monthly_sales = matching_rows["sales price"].sum()
+    #now adding in product name and monthly sales
+    top_sellers.append(
+        {"name": product_name, "monthly_sales": product_monthly_sales})
+
+print(top_sellers)
